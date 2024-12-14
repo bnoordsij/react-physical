09582bb802bf77a29962a670b5a39cadcd0b135f
bart.noordsij <bart.noordsij@radancy.com>
2024-12-14 20:42:16 +0100

    use degrees to show more useful data, get screenwith for dot position

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
   const [data, setData] = useState({ x: 0, y: 0, z: 0 });
+  const screenWidth = Dimensions.get('window').width;
+  // const dotPosition = (data.x + 1) * (screenWidth / 2);
 
   useEffect(() => {
     Accelerometer.setUpdateInterval(100);  // Update data every 100 milliseconds
@@ -17,9 +19,9 @@ export default function App() {
 
   return (
       <View style={styles.container}>
-        <Text style={styles.text}>X: {data.x.toFixed(2)}</Text>
-        <Text style={styles.text}>Y: {data.y.toFixed(2)}</Text>
-        <Text style={styles.text}>Z: {data.z.toFixed(2)}</Text>
+        <Text style={styles.text}>X: {Math.round(data.x.toFixed(2) * 90)}</Text>
+        <Text style={styles.text}>Y: {Math.round(data.y.toFixed(2) * 90)}</Text>
+        <Text style={styles.text}>Z: {Math.round(data.z.toFixed(2) * 90)}</Text>
       </View>
   );
 }
